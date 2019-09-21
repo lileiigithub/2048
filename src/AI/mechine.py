ca804f17f28931e6f0f0fe1872e84f3e073c6662
lileiigithub <lilei_mymail@foxmail.com>
2019-09-21 13:13:53 +0800

    test for stash

--- src/AI/mechine.py
+++ src/AI/mechine.py
@@ -2,6 +2,12 @@ import tensorflow as tf
 from tf.nn import conv2d
 from src.game.puzzle import GameGrid
 
+# test for cherry-pick
+# after stash
+# stash s2
+# stash save 1
+
 if __name__ == '__main__':
     gamegrid = GameGrid()
+
 
